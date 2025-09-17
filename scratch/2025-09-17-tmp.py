0603400dffac5b22ae217522191074bd341b2c61
Nick Vincent <nickmvincent@gmail.com>
2025-09-17 11:48:53 -0700

    add scratch

---
+++ scratch/2025-09-17-tmp.py
@@ -0,0 +1,46 @@
+# # Scratch for Sep 17, 2025
+print("abc")
+
+print("a" + "b" + "c")
+
+x = "abc"
+print(x)
+
+my_list = ["a", "b", "c"]
+out = ""
+for char in my_list:
+    out = out + char
+print(out)
+
+
+# we're at 10:44am 
+
+#.   01234
+x = "hello"
+print("this is x", x)
+print("this is 1", x[1])
+print("this is 1:3", x[1:3])
+print("this is 1:4", x[1:4])
+
+s = "abc"
+l = ["a", "b", "c"]
+
+print( s + "d")
+l = l + ["d"]
+print(l)
+#.   0 1 2
+l = [1,2,3]
+l[1] = 100
+print(l)
+
+s = "123"
+s[1] = "100"
+
+l = ["a", "b"]
+print(l[0] + l[1])
+#.   -4   -3  -2  -1 
+#.   0  1  2  3
+x = 2+2
+
+print(f"my cool string {x} asdasdsa")
+print("my cool string" + " " + x +   " " + "asdasdsa")
